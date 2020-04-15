ec9db9d6afeea0e37115320e6276c1bd4b9c0ad1
wmatt <matt@weingarten.org>
2020-04-15 18:17:19 +0200

    version 2 of task2 a, accuracy around 20 percent

--- task2/main.py
+++ task2/main.py
@@ -8,7 +8,13 @@ import numpy as np
 import math
 from sklearn import svm
 from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multioutput import ClassifierChain
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn import linear_model
 import time
+from sklearn.preprocessing import StandardScaler
+
 
 np.set_printoptions(precision=5,suppress=True, linewidth=300)
 
@@ -17,22 +23,35 @@ np.set_printoptions(precision=5,suppress=True, linewidth=300)
 
 
 
+def sigmoid(x):
+    return 1/(1 + math.exp(-x))
 
 
-
+def map_sigmoid(X):
+    n,m = X.shape
+    for i in range(n):
+        for j in range(m):
+            X[i][j] = sigmoid(X[i][j])
+    return X
 # add logic how to best fill in Nans
 ##add lots more to do (like add interpolation for missing in betweens
 # and other criteria for a whole missing colums)
 def missing_values(raw):
     n,m = raw.shape
     means = np.nanmean(raw,axis=0)
+    stds = np.nanstd(raw,axis=0)
+
     for j in range(m):
+
         for i in range(n):
             if (math.isnan(raw[i][j])):
                 # for mean:
-                # raw[i][j] = means[j]
+                raw[i][j] = means[j]
                 #for zero
-                raw[i][j] = 0
+                # raw[i][j] = 0
+        scaler = StandardScaler(copy=False)
+        scaler.fit(raw)
+        raw = scaler.transform(raw)
     return raw
 
 
@@ -76,7 +95,7 @@ def restructure_predict(raw_results):
     return result
 
 def train_model(samples,labels):
-    return MultiOutputClassifier(svm.SVC(kernel='linear',decision_function_shape='ovr',probability=True)).fit(samples,labels)
+    return OneVsRestClassifier(svm.SVC(kernel='linear',decision_function_shape='ovr',probability=False,max_iter=10000)).fit(samples,labels)
 
 
 def predict_csv(clf):
@@ -86,26 +105,39 @@ def predict_csv(clf):
     return predict(data,clf)
     return restructure_predict(clf.predict_proba(data))
 
-def predict(clf,data ):
-    return restructure_predict(clf.predict_proba(data))
+# def predict_proba(clf,data):
+#     return restructure_predict(clf.predict_proba(data))
+
+def predict_sigmoid(clf,data):
+    return map_sigmoid(clf.decision_function(data))
 
 
+def predict(clf,data):
+    return clf.predict(data)
 
+def score(clf,tests,labels):
+    return clf.score(tests,labels)
 
 
 
 
 def main():
+    set_up_start = time.time()
     samples,labels = setup()
     print("done setup")
     train_start = time.time()
-    clf = train_model(samples[0:500],labels[0:500])
+    setup_duration = train_start - set_up_start
+    print "Preprocessing takes: %.1f second"  % setup_duration
+    train_start = time.time()
+    clf = train_model(samples[0:1000],labels[0:1000])
     print("done training")
-    duration = time.time() - train_start
-    print "Training takes: %.2f second"  % duration
-    print(predict(clf,samples[1001:1005]))
-    compare = labels[5001:5005]
-    print(compare)
+    train_duration = time.time() - train_start
+    print "Training takes: %.1f second"  % train_duration
+    print(predict_sigmoid(clf,samples[10001:10010]))
+    print(predict(clf,samples[10001:10010]))
+    print(labels[10001:10010])
+    print(score(clf,samples[15000:18000],labels[15000:18000]))
+
 
 
 
@@ -120,7 +152,7 @@ def main():
 #      y = np.array([
 #                     [1,0,1,0,1],
 #                     [0,1,0,1,0],
-#                     [0,1,0,1,0]
+#                     [0,0,0,0,1]
 #                   ])
 #
 #      p = np.array([
@@ -128,12 +160,18 @@ def main():
 #                         [2,3,27,155,18],
 #                         [4,1,11,110,28]
 #                    ])
-#      inner_clf = svm.SVC(kernel='linear',probability=True,decision_function_shape='ovo')
+#      print(X)
+#      scaler = StandardScaler(copy=False)
+#      scaler.fit(X)
+#      X = scaler.transform(X)
+#      print(X)
 #
-#      clf = MultiOutputClassifier(inner_clf).fit(X,y)
-#      raw_res = clf.predict_proba(p)
+#      inner_clf = svm.SVC(kernel='linear',probability=False,decision_function_shape='ovo')
+#
+#      clf = OneVsRestClassifier(inner_clf).fit(X,y)
+#      raw_res = map_sigmoid(clf.decision_function(p))
 #      print(raw_res)
-#      print(restructure_predict(raw_res))
+#      # print(restructure_predict(raw_res))
 #
 #      print(clf.predict(p))
 
