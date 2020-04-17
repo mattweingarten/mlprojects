335f52a23373fd60c709d4f5a5ccce539879e9b7
wmatt <matt@weingarten.org>
2020-04-17 14:28:18 +0200

    Adds PCA preprocessing

--- task2/main.py
+++ task2/main.py
@@ -14,11 +14,53 @@ from sklearn.multiclass import OneVsOneClassifier
 from sklearn import linear_model
 import time
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 np.set_printoptions(precision=5,suppress=True, linewidth=300)
 
+# FOLLOWING FUNCTION ARE HELPER METHODS ASSIST PREPROCESSING:
+# LINEAR INTERPOLATION (ZEROING IF NO VALUES)
+# SIGMOID FUNCTION
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+def check_all_nan(vector):
+    checker = np.vectorize(np.isnan)
+    return np.all(checker(vector))
 
+def zeroize(vector):
+    for i in range(vector.size):
+        vector[i] = 0
+    return vector
+
+
+def get_next_val(vector,index):
+    count = 0
+    for j in range(vector.size - index):
+        if(np.isnan(vector[index + j]) == False):
+            return (count,vector[index + j])
+        count += 1
+    return (count,np.nan)
+
+def interp(vector):
+   if(check_all_nan(vector)):
+       return zeroize(vector)
+   prev_val = np.nan
+   for i in range(vector.size):
+        nans,next_val = get_next_val(vector, i)
+        if(np.isnan(vector[i])):
+            if(np.isnan(prev_val)):
+                vector[i] = next_val
+            elif(np.isnan(next_val)):
+                vector[i] = prev_val
+            else:
+                temp = prev_val +  (next_val - prev_val)/ (nans + 1)
+                vector[i] = temp
+                prev_val = temp
+        else:
+            prev_val = vector[i]
+   return vector
 
 
 
@@ -69,10 +111,33 @@ def flatten(raw_data):
 
     return res
 
+def patient_pca(data):
+    pca = PCA(n_components=1).fit(data)
+    # print(pca.singular_values_)
+    # print(pca.explained_variance_ratio_)
+    return pca.components_[0]
+
+def flatten_pca(raw_data):
+    n,w = raw_data.shape
+    res = np.zeros((n/12,w-2))
+    temp = np.zeros((12,w-3))
+    for i in range(n/12):
+        temp = raw_data[(12 * i): (12 *(i+1))][:,3:w]
+        for j in range (w-3):
+            temp[:,j] = interp(temp[:,j])
+
+        res[i][0] = raw_data[i * 12][2] /100
+        res[i][1:w-2] = patient_pca(temp)
+    return res
+
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+
 def setup():
         raw_data = np.genfromtxt("./data/train_features.csv",delimiter=",",skip_header=1)
-        raw_data = missing_values(raw_data)
-        samples = flatten(raw_data)
+        # raw_data = missing_values(raw_data)
+        samples = flatten_pca(raw_data)
         raw_labels = np.genfromtxt("./data/train_labels.csv",delimiter=",",skip_header=1)
         labels = raw_labels[:,1:11]
         return samples,labels
@@ -94,8 +159,29 @@ def restructure_predict(raw_results):
             result[i][j] = raw_results[j][i][0]
     return result
 
+
+
+#
+# parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10], 'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+# searcher = GridSearchCV(clf, parameters,scoring='roc_auc', cv=10,return_train_score=True)
+# searcher.fit(train_array,y_train)
+#
+# # Report the best parameters
+# print("Best CV params", searcher.best_params_)
+#
+# y_test_label=searcher.predict(train_array)
+# print(y_test_label)
+# print(searcher.best_score_)
+
+
 def train_model(samples,labels):
-    return OneVsRestClassifier(svm.SVC(kernel='linear',decision_function_shape='ovr',probability=False,max_iter=10000)).fit(samples,labels)
+    return OneVsRestClassifier(svm.SVC(
+                                kernel='linear',
+                                decision_function_shape='ovr',
+                                probability=False,
+                                gamma=0.1,
+                                C=1,
+                                max_iter=10000)).fit(samples,labels)
 
 
 def predict_csv(clf):
@@ -119,7 +205,8 @@ def score(clf,tests,labels):
     return clf.score(tests,labels)
 
 
-
+def statistics():
+    print("hello")
 
 def main():
     set_up_start = time.time()
@@ -130,15 +217,23 @@ def main():
     setup_duration = train_start - set_up_start
     print "Preprocessing takes: %.1f seconds\n--------------------------------"  % setup_duration
     train_start = time.time()
-    clf = train_model(samples[0:2000],labels[0:2000])
+    clf = train_model(samples[0:10000],labels[0:10000])
     print("--------------------------------\ndone training")
-    train_duration = time.time() - train_start
+    scoring_start = time.time()
+    train_duration = scoring_start - train_start
     print "Training takes: %.1f seconds\n--------------------------------"  % train_duration
+
     # print(predict_sigmoid(clf,samples[10001:10010]))
     # print(predict(clf,samples[10001:10010]))
     # print(labels[10001:10010])
     #score is very harsh since it requires all labels to be correct for it to be a valid entry
-    print "--------------------------------\nScore is: %.5f\n--------------------------------" % score(clf,samples[10000:n],labels[10000:n])
+
+    s = score(clf,samples[10000:n],labels[10000:n])
+    scoring_duration = time.time() - scoring_start
+    print("--------------------------------\ndone setup")
+    print "Scoring takes: %.1f seconds" % scoring_duration
+    print "Score is: %.5f\n--------------------------------" % s
+
 
 
 
